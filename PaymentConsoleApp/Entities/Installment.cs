87b4e109d84456bd2ccf8f591fb434953ac5141f
Kaio Teles <kaiolunateles@gmail.com>
2019-09-11 20:55:19 -0300

    Adding project in remote repository.

---
+++ PaymentConsoleApp/Entities/Installment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Entities
+{
+    class Installment
+    {
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+
+        public Installment()
+        {
+        }
+
+        public Installment(DateTime date, double amount)
+        {
+            Date = date;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return  Date.ToString("dd/MM/yyyy") +
+                    " - " +
+                    Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ PaymentConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using Entities;
+using Services;
+
+namespace PaymentConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract Value: ");
+            double value = double.Parse(Console.ReadLine());
+            Console.Write("Enter the number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+
+            Contract contract = new Contract(number, date, value);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.processContract(contract, months);
+
+            Console.WriteLine("Installments: ");
+            foreach(Installment installment in contract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+
+        }
+    }
+}
